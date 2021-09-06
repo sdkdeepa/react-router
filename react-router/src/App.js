6626d2370e9bcf08ce5c5029ed347e0ccee19bc2
sdkdeepa <deepa94086@gmail.com>
2021-09-05 23:11:53 -0700

    added services, history, location route to About route in App.js. Used <Outlet/> in pages.js

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import { Home, About, Events, Contact } from './pages'
+import { Home, About, Events, Contact, Whoops404, Services, CompanyHistory,Location} from './pages'
 
 function App() {
 	return (
 		<div>
 			<Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/events' element={<Events />}/>
-        <Route path='/contact' element={<Contact />}/>
-      </Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />}>
+          <Route path='services' element={<Services />} />
+          <Route path='history' element={<CompanyHistory />} />
+          <Route path='location' element={<Location />} />
+        </Route>
+				<Route path='/events' element={<Events />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='*' element={<Whoops404 />} />
+			</Routes>
 		</div>
 	)
 }
--- react-router/src/pages.js
+++ react-router/src/pages.js
@@ -1,18 +1,48 @@
 import React from 'react'
-
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 export function Home() {
-    return(
-        <div>
-            <h1> [Company Website]</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1> [Company Website]</h1>
+			<nav>
+				<Link to='about'>About</Link>
+				<Link to='events'>Events</Link>
+				<Link to='contact'>Contact</Link>
+			</nav>
+		</div>
+	)
 }
 
 export function About() {
 	return (
 		<div>
 			<h1> [About]</h1>
+            <Outlet />
+		</div>
+	)
+}
+
+export function Services(){
+    return(
+        <div>
+            <h2> Our Services</h2>
+        </div>
+    )
+}
+
+export function CompanyHistory() {
+	return (
+		<div>
+			<h2> Our Company History</h2>
+		</div>
+	)
+}
+
+export function Location() {
+	return (
+		<div>
+			<h2> Our Location</h2>
 		</div>
 	)
 }
@@ -31,4 +61,13 @@ export function Contact() {
 			<h1> [Contact]</h1>
 		</div>
 	)
+}
+export function Whoops404(){
+    let location = useLocation()
+    // console.log(location)
+    return(
+        <div>
+            <h1>Resource not found at {location.pathname} !</h1>
+        </div>
+    )
 }
